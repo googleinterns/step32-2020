aee43ce03c1cb826c49e921fc2872c1a497a983e
Carol Li <caroldotli@gmail.com>
2020-06-23 14:16:56 -0400

    Update frontend components

--- shopsafe-frontend/src/app/app.module.ts
+++ shopsafe-frontend/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ResultsComponent } from './results/results.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ResultsComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LandingComponent]
 })
 export class AppModule { }
