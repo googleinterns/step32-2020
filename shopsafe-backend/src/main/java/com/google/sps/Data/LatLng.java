902f95e4860081173ee76f837a64d28d03d62ecc
Gabriel Stewart <glstewart17@gmail.com>
2020-06-29 20:26:57 +0000

    Create county and latlng classes.

---
+++ shopsafe-backend/src/main/java/com/google/sps/Data/LatLng.java
@@ -0,0 +1,35 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.data;
+
+/** Class contains the latitude and longitude of a location. */
+public final class LatLng {
+
+    private final double latitude;
+    private final double longitude;
+
+    public LatLng(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
