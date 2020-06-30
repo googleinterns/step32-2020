34504420a445ee16d1b02dd920c09b17af3cdc39
Carol Li <caroldotli@gmail.com>
2020-06-30 17:30:57 -0400

    Add checkin service HTTP request

--- shopsafe-frontend/src/app/api.service.ts
+++ shopsafe-frontend/src/app/api.service.ts
@@ -26,4 +26,18 @@ export class ApiService {
     console.error('ApiService::handleError', error);
     return Observable.throw(error);
   }
+
+
+  /**
+   * Creates new check-in for specific store ID
+   * @param storeId ID of the store that the check-in occurs for
+   * @returns Observable 
+   */
+  public createCheckIn(storeId : string): Observable<Object> {
+    return this.http
+      .post(API_URL + '/checkin', storeId)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
 }
