e85f6eb8fc51dcb42bd700abb488a6883ad6f952
Carol Li <caroldotli@gmail.com>
2020-07-09 16:46:24 +0000

    Add pipe for score rounding

---
+++ shopsafe-frontend/src/app/tests/round.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RoundPipe } from '../pipes/round.pipe';
+
+describe('RoundPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RoundPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
