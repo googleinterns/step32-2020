72e82fddf17cece46a9f6e81d3b4eb6cdb5109b7
Carol Li <caroldotli@gmail.com>
2020-07-13 14:19:42 +0000

    Set line chart as injectable

--- shopsafe-frontend/src/app/google-chart/line-chart/line-chart.component.ts
+++ shopsafe-frontend/src/app/google-chart/line-chart/line-chart.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { GoogleChartService } from  '../service/google-chart.service';
+
+@Injectable({
+  providedIn: 'root',
+})
 
 @Component({
   selector: 'app-line-chart',
@@ -10,27 +14,24 @@ export class LineChartComponent implements OnInit {
 
   private gLib: any;
 
-  constructor(
-    private gChartService: GoogleChartService
-  ) {
+  constructor(private gChartService: GoogleChartService) {
     this.gLib = this.gChartService.getGoogle();
     this.gLib.charts.load('current', {'packages': ['corechart', 'table'], callback: this.drawChart.bind(this)});
-
-    // // Callback method triggered to contain the code to create the line chart
-    // this.gLib.charts.setOnLoadCallBack(this.drawChart.bind(this));
   }
 
   ngOnInit(): void {
   }
 
   private drawChart() {
-    let data = this.gLib.visualization.arrayToDataTable([
-      ['Year', 'Sales', 'Expenses'],
-      ['2004',  1000,      400],
-      ['2005',  1170,      460],
-      ['2006',  660,       1120],
-      ['2007',  1030,      540]
-    ]);
+    // let data = this.gLib.visualization.arrayToDataTable([
+    //   ['Year', 'Sales', 'Expenses'],
+    //   ['2004',  1000,      400],
+    //   ['2005',  1170,      460],
+    //   ['2006',  660,       1120],
+    //   ['2007',  1030,      540]
+    // ]);
+
+    let data = new this.gLib.visualization.DataTable();
 
     let chart = new this.gLib.visualization.LineChart(document.getElementById('line-chart'));
 
