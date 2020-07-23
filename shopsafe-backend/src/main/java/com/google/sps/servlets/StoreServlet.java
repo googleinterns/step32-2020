20fb40eff30ce9255fd819774ca85de6478da172
Gabriel Stewart <glstewart17@gmail.com>
2020-07-23 03:20:12 +0000

    Handle store without rating.

--- shopsafe-backend/src/main/java/com/google/sps/servlets/StoreServlet.java
+++ shopsafe-backend/src/main/java/com/google/sps/servlets/StoreServlet.java
@@ -118,7 +118,7 @@ public class StoreServlet extends HttpServlet {
                 result.getString("vicinity"),
                 (result.has("opening_hours")) ? result.getJSONObject("opening_hours").getBoolean("open_now") : null,
                 new LatLng(storeLocation.getDouble("lat"), storeLocation.getDouble("lng")),
-                result.getDouble("rating"));
+                result.has("rating") ? result.getDouble("rating"): 2.5);
         }
 
         // If error, print error, and set status to bad reuqest and send error response.
